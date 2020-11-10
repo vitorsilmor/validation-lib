759fe639ccd6c5d751e9e446b8ca6eba6a1b311f
joaopsv <joao.p1@hotmail.com>
2020-11-10 14:25:39 -0300

    Create class validator with static methods

---
+++ src/Validator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sanar;
+
+class Validator
+{
+    public static function validateCpf($value)
+    {
+        $validator = new Cpf();
+        return $validator->validate($value);
+    }
+
+    public static function validateGeneral($value)
+    {
+        $validator = new General();
+        return $validator->validate($value);
+    }
+
+    public static function validateCep($value)
+    {
+        $validator = new Cep();
+        return $validator->validate($value);
+    }
+
+    public static function validateUf($value)
+    {
+        $validator = new Uf();
+        return $validator->validate($value);
+    }
+
+    public static function validateEmail($value)
+    {
+        $validator = new Email();
+        return $validator->validate($value);
+    }
+}
